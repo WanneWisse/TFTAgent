cc204bc98120967bba09deaab443436647656d4a
WanneWisse <wanne.wisse@gmail.com>
2026-04-14 17:59:12 +0200

    save windowcapture

--- record_screen.py
+++ record_screen.py
@@ -3,6 +3,11 @@ from mss import mss
 import numpy as np
 import cv2
 
+def showFrame(frame, scale):
+    # Resize (scale down to half)
+    scale = 0.3
+    frame = cv2.resize(frame, (0, 0), fx=scale, fy=scale)
+    cv2.imshow("Window Capture", frame)
 # Find window (change title!)
 window = gw.getWindowsWithTitle("League of Legends (TM) Client")[0]
 
@@ -20,18 +25,29 @@ monitor = {
 
 sct = mss()
 
+out_width = 1920
+out_height = 1080
+
+# --- Video writer setup ---
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+out = cv2.VideoWriter("output.mp4", fourcc, 20.0, (out_width, out_height))
+
+
 while True:
     img = sct.grab(monitor)
     frame = np.array(img)
     frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
+    # Resize frame
+    frame = cv2.resize(frame, (out_width, out_height))
 
-     # Resize (scale down to half)
-    scale = 0.3
-    frame = cv2.resize(frame, (0, 0), fx=scale, fy=scale)
-
-    cv2.imshow("Window Capture", frame)
+    showFrame(frame,0.3)
+    # Write to file
+    out.write(frame)
+    
 
     if cv2.waitKey(1) == ord("q"):
         break
 
+# Cleanup
+out.release()
 cv2.destroyAllWindows()
